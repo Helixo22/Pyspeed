4c8b5f1cc1b0ec27ab273c9d8b1ed5a2f24f164a
Capasso Alessio <137064136+Helixo22@users.noreply.github.com>
2023-12-29 23:09:18 +0100

    Fix error in the code
    Fix bug

--- main.py
+++ main.py
@@ -1,27 +1,76 @@
 import tkinter 
 import customtkinter
-import subprocess
+import speedtest
 
 # Function
 def perform_speed_test():
     status_label.configure(text="Running speed test...")
+    progress_bar.set(0)  
 
     try:
-        result = subprocess.run(["speedtest-cli", "--simple"], capture_output=True, text=True)
-        output_lines = result.stdout.split('\n')
+        st = speedtest.Speedtest()
+        
+        
+        status_label.configure(text="Loading server list...")
+        st.get_servers()
+        app.update_idletasks()
 
-        if len(output_lines) >= 2:
-            download_speed = float(output_lines[0].split()[1]) / 1000000  # Convert to Mbps
-            upload_speed = float(output_lines[1].split()[1]) / 1000000  # Convert to Mbps
+       
+        status_label.configure(text="Searching for the best server...")
+        best = st.get_best_server()
+        status_label.configure(text=f"Found: {best['host']} located in {best['country']}")
+        app.update_idletasks()
 
-            # Update the text of finishLabel
-            finishLabel.configure(text="Download Speed: {:.2f} Mbps\nUpload Speed: {:.2f} Mbps".format(download_speed, upload_speed))
-            status_label.configure(text="Speed test completed.")
-        else:
-            finishLabel.configure(text="Error: Unable to retrieve speed test results.")
-            status_label.configure(text="Speed test failed.")
+        status_label.configure(text="Performing download test...")
+        app.update_idletasks()
+        progress_bar.start() 
+
+        def update_progress():
+            progress = progress_bar.get()
+            if progress < 50:
+                progress += 1
+                progress_bar.set(progress)
+                app.after(50, update_progress)  
+            elif progress < 75:
+                progress += 5
+                progress_bar.set(progress)
+                app.after(50, update_progress) 
+            else:
+                app.after(50, lambda: progress_bar.set(50))  
+
+        app.after(50, update_progress) 
+        download_result = st.download()
+
+     
+        status_label.configure(text="Performing upload test...")
+        app.update_idletasks()
+
+        def update_progress_upload():
+            progress = progress_bar.get()
+            if progress < 100:
+                progress += 5
+                progress_bar.set(progress)
+                app.after(50, update_progress_upload)  
+            else:
+                app.after(50, lambda: progress_bar.set(75))  
+
+        app.after(50, update_progress_upload)  
+        upload_result = st.upload()
+
+        
+        ping_result = st.results.ping
+
+      
+        progress_bar.stop()
+        app.update_idletasks()
+
+        
+        finishLabel.configure(text=f"Download speed: {download_result / 1024 / 1024:.2f} Mbit/s\nUpload speed: {upload_result / 1024 / 1024:.2f} Mbit/s\nPing: {ping_result} ms")
+        status_label.configure(text="Speed test completed.")
     except Exception as e:
-        finishLabel.configure(text="Error: {}".format(e))
+       
+        progress_bar.stop()
+        finishLabel.configure(text=f"Error: {e}")
         status_label.configure(text="Speed test failed.")
 
 # System settings
@@ -48,6 +97,10 @@ status_label.pack()
 # SpeedTest Button
 speedtest_button = customtkinter.CTkButton(app, text="Start the test", command=perform_speed_test)
 speedtest_button.pack(padx=10, pady=10)
+
+# Progress bar
+progress_bar = customtkinter.CTkProgressBar(app, width=400)
+progress_bar.pack(pady=10)
 
 # Run App 
 app.mainloop()
